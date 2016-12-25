1581b3b1fcb1fc47682c548e2e74a4470ede2b74
Tigro <ashejn@gmail.com>
2016-12-26 00:05:05 +0300

    ignore VCS folders

--- get_sources.sh
+++ get_sources.sh
@@ -3,5 +3,5 @@ NR=$(rpm -q --specfile *.spec --qf "%{name}-%{version}")
 
 rm -rf $NR
 git clone -q git://github.com/Tigro/mock-configs-rfremix.git $NR
-tar cjf $NR.tar.bz2 $NR --exclude=.git
+tar -ca --exclude-vcs --exclude-vcs-ignores -f $NR.tar.bz2 $NR --exclude=.git
 rm -rf $NR
